b93ce19f62d0d832994c889411f265cd0d5613d0
All298Lie <whdudfo333@naver.com>
2026-04-08 01:36:55 +0900

    기물 스크립트 생성 및 커서 png 형식 변경
    기물 스크립트 생성 및 커서 png 형식 변경

--- Assets/Scripts/Piece/Piece.cs
+++ Assets/Scripts/Piece/Piece.cs
@@ -2,15 +2,45 @@
 
 public class Piece : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    [SerializeField] SpriteRenderer renderer;
+
+    PieceData data;
+
+    Vector2Int currentPosition;
+
+    bool isWhite;
+    bool hasMoved;
+
+
+    // 기물 Sprite를 새로고침 하는 함수
+    //private void RefreshSprite()
+    //{
+    //    if (data == null || ThemeManager.Instance == null) return;
         
+    //    PieceThemeData activeTheme = ThemeManager.Instance.currentPieceTheme;
+    //    if (activeTheme != null)
+    //    {
+    //        renderer.sprite = activeTheme.GetSprite(this.data.type, this.isWhite);
+    //    }
+    //}
+
+    // BoardManager가 기물을 스폰할 때 딱 한 번 호출하는 함수
+    public void Setup(PieceData data, bool isWhite, Vector2Int startPos)
+    {
+        this.data = data;
+        this.isWhite = isWhite;
+        this.currentPosition = startPos;
+        this.hasMoved = false;
+
+        // RefreshSprite(); // 생성 즉시 테마에 맞는 기물 Sprite 입히기
     }
 
-    // Update is called once per frame
-    void Update()
+    // 기물을 이동할 때 호출되는 함수
+    public void MoveTo(Vector2Int newArrayPos, Vector3 newWorldPos)
     {
-        
+        this.currentPosition = newArrayPos;
+        hasMoved = true;
+
+        transform.position = newWorldPos;
     }
 }
