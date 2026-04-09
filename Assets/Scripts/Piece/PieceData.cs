5697d42eb296c8953ea180612919aba1f82cae20
All298Lie <whdudfo333@naver.com>
2026-04-10 02:39:08 +0900

    변수명 오타 수정

--- Assets/Scripts/Piece/PieceData.cs
+++ Assets/Scripts/Piece/PieceData.cs
@@ -7,7 +7,7 @@ public class PieceData : ScriptableObject
     [Header("이동/공격 관련")]
     public List<Vector2Int> moveOffsets; // 이동 오프셋
     public List<Vector2Int> attackOffsets; // 공격 오프셋(이동 오프셋과 공격 오프셋이 다른 폰 때문)
-    public List<Vector2Int> sliedDirections; // 슬라이드 방향 (룩, 비숍, 퀸 같이 막힐때까지 쭉 나아가는 기물을 위한 변수)
+    public List<Vector2Int> slideDirections; // 슬라이드 방향 (룩, 비숍, 퀸 같이 막힐때까지 쭉 나아가는 기물을 위한 변수)
 
     [Header("기타 속성")]
     public int meterialValue; // 기물 점수
