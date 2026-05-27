4b58174401d2d13efa182b5e8143544917fbbdf2
All298Lie <74299604+All298Lie@users.noreply.github.com>
2026-05-27 14:13:55 +0900

    채팅 UI 구성 및 프레임 구현

---
+++ Assets/Scripts/UI/Game/ChatUIController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ChatUIController : MonoBehaviour
+{
+    [SerializeField] private GameObject chatMessagePrefab;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
